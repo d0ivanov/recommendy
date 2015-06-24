b2383da1d34f7ca37c50604a42133da6f6c65e26
d0ivanov <dobromir0ivanov@gmail.com>
2015-06-24 14:31:08 +0300

    Add a json data provider.
    Basic class that will try to a request to a given address and expect a JSON
    response which it will try to parse to a hash.

--- recommendy/data_provider.py
+++ recommendy/data_provider.py
@@ -1,7 +1,18 @@
 class DataProvider:
     """Represents the interface each data provider class should implement."""
-    def fetch_data(self):
-        """Fetches data and returns a tuple in which each data item
-        is associated with a numeric value (rating)."""
-        pass
 
+    def get_data(self):
+        """Return a hash of the parsed data"""
+        raise NotImplementedError
+
+    def _fetch_data(self):
+        """Fetches data and returns it. All items must be comprised of
+        properties that have some numeric values."""
+        raise NotImplementedError
+
+    def _parse_data(self):
+        """Parse fetched data to a hashmap and return it. Each key is a
+        data item. Each data item should have a hashmap of properties and
+        their corresponding numeric values
+        """
+        raise NotImplementedError
--- recommendy/json_data_provider.py
+++ recommendy/json_data_provider.py
@@ -1,4 +1,17 @@
 from data_provider import DataProvider
+import urllib.request
+import json
 
 class JSONDataProvider(DataProvider):
-    pass
+
+    def __init__(self, source):
+        self.source = source
+
+    def get_data(self):
+        return self._parse_data()
+
+    def _fetch_data(self):
+        return urllib.request.urlopen(self.source).read().decode('utf-8')
+
+    def _parse_data(self):
+        return json.loads(self._fetch_data())
