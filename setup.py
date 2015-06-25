26191b4e70f5acdce41e9cfbff19acbb9d2d8bba
d0ivanov <dobromir0ivanov@gmail.com>
2015-06-26 00:49:08 +0300

    setup script & detailed description of the project (work in progress)

--- setup.py
+++ setup.py
@@ -70,8 +70,6 @@ setup(
     # "scripts" keyword. Entry points provide cross-platform support and allow
     # pip to create the appropriate form of executable for the target platform.
     entry_points={
-        'console_scripts': [
-            'main = recommendy:main'
-        ]
+        'console_scripts':[]
     }
 )
