ca020778cf17cf6eba69c94c83fbc0ee86efc811
d0ivanov <dobromir0ivanov@gmail.com>
2015-06-26 15:31:46 +0300

    Refactor the data provider. Add functionality to transpose data

--- recommendy/data_provider.py
+++ recommendy/data_provider.py
@@ -1,18 +1,19 @@
 class DataProvider:
     """Represents the interface each data provider class should implement."""
 
+    def __init__(self, source):
+        self._source = source
+
     def get_data(self):
         """Return a hash of the parsed data"""
         raise NotImplementedError
 
-    def _fetch_data(self):
-        """Fetches data and returns it. All items must be comprised of
-        properties that have some numeric values."""
-        raise NotImplementedError
-
-    def _parse_data(self):
-        """Parse fetched data to a hashmap and return it. Each key is a
-        data item. Each data item should have a hashmap of properties and
-        their corresponding numeric values
-        """
-        raise NotImplementedError
+    def transpose_data(self):
+        """Transpose the data so that properties become keys
+        and keys become properties."""
+        result = {}
+        for key, value in self.get_data().items():
+            for property, score in value.items():
+                result.setdefault(property, {})
+                result[property][key] = score
+        return result
